d284db9241d91dded57f3fb9adbd173d56dcc85d
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-03-31 01:47:23 +0530

    leap year check

---
+++ LEAP_YEAR_CHECK.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+int y,x;
+printf("type a year");
+scanf("%d",&y);
+x=y%4 ;
+printf("%d%d",x,y);
+x>0?printf("not leap"):printf("leap"); // CONDITIONAL OPERATOR
+return 0 ;
+}
