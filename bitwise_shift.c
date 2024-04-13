fbdee15529eb0c7f841476da6b93e355931c9de7
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-04-13 21:25:57 +0530

    Create bitwise_shift.c

---
+++ bitwise_shift.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main () {
+int x=23>>2 ;
+printf("%d",x);
+return 0 ;
+}
+// X=23>>X MEANS X 0 WERE ADDED IN BINARY FORM FROM RIGHT SIDE
+// X=23<<X MEANS X 0 WERE ADDED IN BINARY FORM FROM LEFT SIDE
