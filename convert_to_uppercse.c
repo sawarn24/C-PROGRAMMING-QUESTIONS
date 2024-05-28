4f1923aa7f532afb332026311cf6caa286ed82a8
sawarn24 <150719088+sawarn24@users.noreply.github.com>
2024-05-28 23:34:16 +0530

    Add files via upload

--- convert_to_uppercse.c
+++ convert_to_uppercse.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void to_upper(char* str) {
+void to_upper(char *str) {
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] >= 'a' && str[i] <= 'z') {
             str[i] = str[i] - ('a' - 'A');
